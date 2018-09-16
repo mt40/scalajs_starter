0ad39091f9184e8670dd1341afc419cb45e83fd7
minhthai <minhthai40@gmail.com>
2018-09-16 16:32:51 +0800

    Compatibility with Github Page

--- src/main/scala/AppRouter.scala
+++ src/main/scala/AppRouter.scala
@@ -12,15 +12,24 @@ object AppRouter {
   private val routerConfig = RouterConfigDsl[PageRepr].buildConfig { dsl =>
     import dsl._
 
+    // Github Page serves "/scalajs_starter" not "/"
+    val publicUrl = sys.env.getOrElse("PUBLIC_URL", "")
+    val siteRoot = root / publicUrl
+
     // the use of `emptyRule` is just for nice formatting
-    (trimSlashes
+    (emptyRule
     // "/" -> home page
-      | staticRoute(root, Home) ~> render(HomePage()))
+      | staticRoute(siteRoot, Home) ~> render(HomePage())
+      | staticRedirect(root) ~> redirectToPage(Home)(Redirect.Replace))
     // not found -> home page
       .notFound(redirectToPage(Home)(Redirect.Replace))
   }
 
   def apply() = {
+
+    // REMOVE
+    routerConfig.logToConsole
+
     val baseUrl = BaseUrl.fromWindowOrigin
     Router(baseUrl, routerConfig)()
   }
